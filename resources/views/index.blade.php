af040dab21b6795077ddd0dee74fba083bb23855
BayuMagawan <bayumagawan9@gmail.com>
2019-04-03 14:57:08 +0700

    modul-4 routes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-		<title>Laravel Saya</title>
+<title>Laravel Saya</title>
 </head>
 <body>
-	<h1>Produk : {{ isset($produk) ? $produk : 'Produk Kosong' }}</h1>
+<h1>Produk : {{ $produk }}</h1>
 </body>
 </html>
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,1 @@
+<marquee behavior ><h1>Layout Dengan Blade Template Laravel</marquee behavior></h1>
